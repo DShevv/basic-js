43908d1362db94e883ff1337a2a7a499a872caf7
Dmitry Shevtsov <dshevtsov20@gmail.com>
2020-10-30 20:11:50 +0300

    feat: complite whatSeason task

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
+const seasons = {
+  1: 'winter',
+  2: 'winter',
+  3: 'spring',
+  4: 'spring',
+  5: 'spring',
+  6: 'summer',
+  7: 'summer',
+  8: 'summer',
+  9: 'fall',
+  10: 'fall',
+  11: 'fall',
+  12: 'winter',
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+}
+
+module.exports = function getSeason(date) {
+  if(date instanceof Date && !isNaN(date.valueOf())){
+    return seasons[date.getMonth()+1];
+  }
+  else{
+    if(date == null){
+      return 'Unable to determine the time of year!';
+    }
+    else{
+      throw 'FAIL';
+    }
+    
+  }
 };
