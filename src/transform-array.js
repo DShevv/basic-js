1bd5487e29c7fade41e4c9eed1766050c629f1cf
Dmitry Shevtsov <dshevtsov20@gmail.com>
2020-10-30 22:28:35 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,59 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw 'ERROR'
+  }
+  var cock = []
+  for (let index = 0; index < arr.length; index++) {
+    if (typeof arr[index] == 'string') {
+      if(arr[index].split('-')[2] == 'discard'){
+        if (arr[index].split('-')[3] == 'prev') {
+          if(index != 0){
+            cock[index-1] = null;
+            cock.push(null);
+          }
+          else{
+            cock.push(null);
+          }           
+        }
+        else{
+          cock.push(null);
+          cock.push(null);
+          index++;
+        }
+      }
+      else{
+        if (arr[index].split('-')[3] == 'prev') {
+          if(index != 0 && cock[index-1] != null){
+            cock.push(arr[index-1]);
+          }
+          else{
+            cock.push(null);
+          }
+        }
+        else{
+          if(index != arr.length-1){
+            cock.push(arr[index+1]);
+          }
+          else{
+            cock.push(null);
+          }
+        }
+      }
+    }
+    else{
+      cock.push(arr[index]);   
+    }
+    
+  }
+  var final = [];
+  cock.forEach(element => {
+    if (element != null) {
+      final.push(element);
+    }
+  });
+    
+
+  return final
 };
